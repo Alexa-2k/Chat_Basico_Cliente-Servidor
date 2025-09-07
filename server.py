c77512ab3e23b1c0ee499bb79e0476aab2c75408
Alexa-2k <subscrip.lr@gmail.com>
2025-09-06 22:03:52 -0300

    mejoras en los comentarios

--- server.py
+++ server.py
@@ -2,9 +2,12 @@ import socket
 import sqlite3
 from datetime import datetime
 
-# --- Base de datos ---
+# --- Creación de la base de datos y la tabla de mensajes ---
 def inicializar_db():
     """Crea la base de datos y la tabla si no existen"""
+    
+    
+# --- Crear tabla con id, contenido, fecha e IP. Si no hay errores, da el mensaje "Base de datos lista" ---
     try:
         conn = sqlite3.connect("chat.db")
         cursor = conn.cursor()
@@ -19,10 +22,13 @@ def inicializar_db():
         conn.commit()
         conn.close()
         print("Base de datos lista.")
+        
+    # --- Si hay errores, da el mensaje "Error al inicializar la base de datos" ---
     except sqlite3.Error as e:
         print(f"Error al inicializar la base de datos: {e}")
         exit(1)
 
+# --- Guarda un mensaje en la base de datos y devuelve la confirmación con la fecha de envío ---
 def guardar_mensaje(contenido, ip_cliente):
     """Guarda un mensaje en la base de datos y devuelve la fecha de envío"""
     try:
@@ -34,6 +40,7 @@ def guardar_mensaje(contenido, ip_cliente):
         conn.commit()
         conn.close()
         return fecha_envio
+  # --- Si hay algún error, da el mensaje "Error al guardar el mensaje" ---
     except sqlite3.Error as e:
         print(f"Error al guardar el mensaje: {e}")
         return None
@@ -62,17 +69,17 @@ def manejar_conexiones(server_socket):
             if not data:
                 break  # si no hay datos, se corta la conexión
 
-            # Guardar mensaje en la DB
+         # --- Guardar mensaje en la DB ---
             fecha_envio = guardar_mensaje(data, client_address[0])
             if fecha_envio:
                 respuesta = f"Mensaje recibido: {fecha_envio}"
             else:
                 respuesta = "Error al guardar el mensaje"
 
-            # Enviar respuesta al cliente
+            # --- Enviar respuesta al cliente ---
             client_socket.send(respuesta.encode("utf-8"))
 
-            # Si el cliente envía "salida", se cierra esa conexión
+            # --- Si el cliente envía "salida", se cierra esa conexión ---
             if data.lower() == "salida":
                 print("Cliente finalizó la sesión.")
                 break
